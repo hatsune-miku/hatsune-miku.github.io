8d9f2fb82bcf617ff5f2f7c210d2cdb296fae8bc
Hatsune Miku <l2a1knla@gmail.com>
2024-01-23 09:56:40 -0800

    Add hits statistics

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,25 @@
 import styles from "./Header.module.scss"
 import { Chip } from "@mui/joy"
 import LinkAirX from "./LinkAirX"
+import { useState } from "react"
 
 export default function Header() {
+    const [counter, setCounter] = useState(0)
+
+    function handleClickOnLatestProject() {
+        setCounter(counter + 1)
+    }
+
     return <>
         <div className={styles.header}>
-            <Chip>LATEST PROJECT</Chip>
+            <a href="https://hits.seeyoufarm.com" style={{ display: counter > 10 ? 'flex' : 'none' }}>
+                <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fhatsune-miku&count_bg=%237CD2FF&title_bg=%23FF98D8&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false" />
+            </a>
+            <Chip onClick={handleClickOnLatestProject}>LATEST PROJECT</Chip>
             &nbsp;
-            See how <LinkAirX /> blurs the boundary between devices and platforms!
+            <span>
+                See how <LinkAirX /> blurs the boundary between devices and platforms!
+            </span>
         </div>
     </>
 }
