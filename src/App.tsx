f33b18d9304ec08b85f54fcc4ed0289d44455efe
Hatsune Miku <l2a1knla@gmail.com>
2024-09-11 23:37:04 +0800

    miku

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { Box, Stack, useColorScheme } from '@mui/joy'
-import './App.css'
-import Header from './components/Header'
+import { Box, Stack, useColorScheme } from "@mui/joy"
+import "./App.css"
+import Header from "./components/Header"
 
-import Portfolio from './components/Portfolio'
-import Footer from './components/Footer'
-import ContactInfo from './components/ContactInfo'
-import Education from './components/Education'
-import ArchivedProjects from './components/ArchivedProjects'
-import { useEffect } from 'react'
-import ActiveProjects from './components/ActiveProjects'
-import useSystemDarkMode from './utils/is_darkmode'
+import { useEffect } from "react"
+import useSystemDarkMode from "./utils/is_darkmode"
 
 function App() {
   const isDarkMode = useSystemDarkMode()
@@ -17,23 +11,20 @@ function App() {
 
   // Sync dark mode with MUI
   useEffect(() => {
-    setMode(isDarkMode ? 'dark' : 'light')
+    setMode(isDarkMode ? "dark" : "light")
   }, [isDarkMode, setMode])
 
   return (
-    <Box sx={{ display: 'flex' }} alignContent="center" justifyContent="center">
-      <Stack direction="column" sx={{
-        maxWidth: '1200px',
-      }}>
+    <Box sx={{ display: "flex" }} alignContent="center" justifyContent="center">
+      <Stack
+        direction="column"
+        sx={{
+          maxWidth: "1200px",
+        }}
+      >
         <Header />
-        <Portfolio />
-        <Education />
-        <ActiveProjects />
-        <ArchivedProjects />
-        <ContactInfo />
-        <Footer />
       </Stack>
-    </Box >
+    </Box>
   )
 }
 
